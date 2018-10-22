83598fe557932de591e80a3ffb96f8b1e4d1c681
Josh Hawkins <hawkins@users.noreply.github.com>
2018-10-21 22:40:53 -0400

    Inspect folders too

--- lib/lard.rb
+++ lib/lard.rb
@@ -16,10 +16,17 @@ class Lard
     get 'user'
   end
 
+  def folders(limit = nil, offset = nil)
+    params = { limit: limit, offset: offset }
+    get 'folders', params
+  end
+
   private
 
-  def get(endpoint)
-    res = self.class.get "#{prefix}#{endpoint}", @options
+  def get(endpoint, params = nil)
+    query = { query: params }
+    opts = @options.merge query
+    res = self.class.get "#{prefix}#{endpoint}", opts
     JSON.parse res.body, symbolize_names: true
   end
 
